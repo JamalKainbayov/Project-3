9f741a0bbee505d90b35ea4f00cf3a56c6501985
Jamal Kainbayov <9023725@student.zadkine.nl>
2024-03-08 13:31:07 +0100

    css added

--- Login.php
+++ Login.php
@@ -6,20 +6,23 @@ require "conn.php"
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/main.css">
+
     <title>Login</title>
 </head>
+<style>
+    <?php include "Css/main.css"?>
+</style>
 <body>
-    <form method="POST" action="Login.php" class=>
-        <label for="username">Username:</label>
+   
+<form method="POST" action="Login.php" class= "login">
+    <label for="username">Username:</label>
         <input type="text" name="username" autocomplete="off"><br>
-        <label for="password">Password:</label>
+    <label for="password">Password:</label>
         <input type="password" name="password" required><br>
         <a class=JxN href="#"> Forgot password?<a>
         <input type="submit" value="Log in">
-         <button type="submit">Register</button>
-</form>
-    
+        <button type="submit">Register</button>
+</form>  
 </body>
 <?php 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {

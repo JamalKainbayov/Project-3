0f17f7dd0b3fe4472024e20a1a249ba18e47c103
Jamal Kainbayov <9023725@student.zadkine.nl>
2024-03-13 10:45:45 +0100

    Register done

---
+++ Register.php
@@ -0,0 +1,42 @@
+<?php
+require "conn.php"
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Login</title>
+</head>
+<style>
+    <?php include "Css/main.css"?>
+</style>
+<body>
+   
+<form method="POST" action="Register.php" class= "login">
+    <label for="username">Username:</label>
+        <input type="text" name="username" autocomplete="off"><br>
+    <label for="password">Password:</label>
+        <input type="password" name="password" required><br>
+        <a class=JxN href="#"> Forgot password?<a>
+        <input type="submit" value="Log in">
+        <button type="submit">Register</button>
+</form>  
+</body>
+<?php 
+$encrypt_password = password_hash($_POST, PASSWORD_DEFAULT);
+
+$insert_user = $conn->prepare("INSERT INTO user_info (username, password) VALUES (:gebruikersnaam, :wachtwoord)");
+$insert_user->bindParam(":gebruikersnaam", $_POST['username']);
+$insert_user->bindParam(":wachtwoord", $encrypt_password);
+$insert_user->execute();
+
+if ($insert_user->execute()){
+
+    header("Location: Login.php");
+}
+
+
+?>
+</html>

cd2f9a459b406a262791cb08d9d43c2b0d59b8a9
Raghda <fullasta151@gmail.com>
2024-03-25 21:47:18 +0100

    add style, comments

--- Index.php
+++ Index.php
@@ -1,6 +1,6 @@
 <?php
 
-
+include("db.php");
 
 ?>
 
@@ -9,8 +9,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/main.css">
+    <link rel="stylesheet" href="./css/main.css">
     <title>Chirpfy</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/emojionearea/3.4.2/emojionearea.min.css" />
+
+	<link rel="stylesheet" href="./css/tweet_box.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+	
 </head>
 <body>
  <?php require "Nav.php" ?>

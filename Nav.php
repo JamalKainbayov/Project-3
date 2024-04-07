a30ee3c7aa011d51c4fd0d44f61b525aaadd684b
zeroku <scheepstrabas@icloud.com>
2024-04-07 09:44:14 +0200

    error oplossen
    nog steeds aan het testen

--- Nav.php
+++ Nav.php
@@ -22,7 +22,7 @@
                 <span class="tooltip">Home</span>
             </li>
             <li>
-                <a href="profiel.php">
+                <a href="profiel.html">
                     <i class='bx bx-user'></i>
                     <span class="links_name">Profile</span>
                 </a>

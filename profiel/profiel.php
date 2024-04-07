8e70a8e6c0c24bd3d71d5df946da2a34fb5a9c42
zeroku <scheepstrabas@icloud.com>
2024-04-07 09:31:03 +0200

    update profiel

--- profiel/profiel.php
+++ profiel/profiel.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="main.css">
-    <link rel="stylesheet" href="style2.css">
+    <link rel="stylesheet" href="profiel.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     
 <script> 
@@ -17,35 +16,6 @@ function changeButtonText()
 </script>
 </head>
 <body>
-    <div class="menu">
-        <img src="images/xxx.jpeg" alt="porofile">
-        <Ul> 
-            <li class="active"> <div class="img-box"> <img src="xxx.jpeg" alt="porofile">
-    </div>
-    </li>
-    <li>
-        <a href="home.php">
-        <i class="fas fa-home"></i> 
-        <p>Home</p> </a></li>
-        <li> <a href="#"> <i class="fas fa-search"></i>
-        <p>Explore</p> </a> </li> 
-        <li><a href="#"> <i class="fa-regular fa-bell"></i>
-        <p>Notififcations</p> </a> </li> 
-        <li><a href="#"> <i class="fa-regular fa-message"></i>
-        <p>Masseges</p> </a> </li>
-        <li><a href="#"> <i class="fa-solid fa-list"></i>
-        <p>Lists</p> </a> </li>
-        <li> <a href="#"> <i class="fa-solid fa-group-arrows-rotate"></i>
-        <p>Communities</p> </a> </li>
-        <li> <a href="#"> <i class="fa-solid fa-xmark"></i>
-        <p>Premium</p> </a> </li>
-        <li> <a href="#"> <i class="fa-light fa-user"></i>
-        <p>Profile</p> </a> </li>
-        <li class="log-out"> <a href="index.html">
-        <p>Post</p> </a> </li>
-        <li class="log-out">
-            <a href="logout.php" class="btn btn-warning">Logout</a>
-        <i class="fas fa-sign-out"></i> </a> </li> </Ul> </div>
     <div class="content">
     <div class="center">
         <a href="#"> <i class="fa-solid fa-arrow-left"></i></a>

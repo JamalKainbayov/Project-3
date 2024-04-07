236bd6a305a214de33ce95407160b452db7fb6d5
zeroku <scheepstrabas@icloud.com>
2024-04-07 09:42:30 +0200

    update profiel
    html errors aan het fixen

--- profiel/profiel.php
+++ profiel/profiel.php
@@ -6,7 +6,17 @@
     <title>Document</title>
     <link rel="stylesheet" href="profiel.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    
+</head>
+<body>
+    <div class="content">
+    <div class="center">
+        <a href="#"> <i class="fa-solid fa-arrow-left"></i></a>
+        <h3>Koko Tengaa Po</h3> </div> <div class="grijs">
+            <img src="images/profiel.png" alt="profiel foto"> </div>
+            <button class="edit-profile-btn">Edit Profile</button>
+        <div class="profile7"> <h3>Username</h3> <h5>E-mail</h5> <h7>Joined January 2019</h7>
+        <div class="button-container"> <button class="button">Following: 69</button>
+        <button class="button">Followers: 4</button> </div>
 <script> 
 function changeButtonText() 
 { var fileInput = document.getElementById("imageInput"); 
@@ -14,17 +24,5 @@ function changeButtonText()
         if (fileInput.files.length > 0) { submitButton.value = "Verander je foto"; }
             else { submitButton.value = "Upload Image"; } }
 </script>
-</head>
-<body>
-    <div class="content">
-    <div class="center">
-        <a href="#"> <i class="fa-solid fa-arrow-left"></i></a>
-        <h3>Koko Tengaa Po</h3> </div> <div class="grijs">
-            <img src="images/mounty.jpg" alt="porofile"> </div>
-            <button class="edit-profile-btn">Edit Profile</button>
-        <div class="profile7"> <h3>Koko Tengaa Po</h3> <h5>marjer@gmail.com</h5> <h7>Joined January 2019</h7>
-        <div class="button-container"> <button class="button">Following: 23</button>
-        <button class="button">Followers: 1</button> </div> </div>
-        <div class="nap"> <img src="images/testen.png" alt="porofile"> </div>
-    </body>
+</body>
 </html>

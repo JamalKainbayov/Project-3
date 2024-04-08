3f9f8b10d81c8513db0f09957f80f14d6ccd01e0
zeroku <scheepstrabas@icloud.com>
2024-04-08 11:03:06 +0200

    updates profiel

--- profiel.php
+++ profiel.php
@@ -1,12 +1,13 @@
+
+<?php require "Index.php" ?>
 <!DOCTYPE html> 
 <html lang="en">
-<?php require "Nav.php" ?>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="profiel.css">
-    <link rel="stylesheet" href="main.css">
 
 </head>
 <body>
@@ -15,7 +16,7 @@
         <a href="#"> <i class="fa-solid fa-arrow-left"></i></a>
         <h3>Username</h3> </div> <div class="grijs">
             <img src="profiel.png" alt="profiel foto"> </div>
-            <button class="edit-profile-btn">Edit Profile</button>
+            <a href="update.profie.php"> <button class="edit-profile-btn">Edit Profile</button></a>
         <div class="profile7"> <h3>Username</h3> <h5>E-mail</h5> <h7>Joined January 2019</h7>
         <div class="button-container"> <button class="button">Following: 69</button>
         <button class="button">Followers: 4</button> </div>

b913eaafb6aa091ac1cf21b6aef5dc4aa7fec9b9
Ilie <9015672@student.zadkine.nl>
2024-04-12 08:51:14 +0200

    adminpage gemaakt
    adminpage gemaakt

---
+++ adminDashboard.php
@@ -0,0 +1,36 @@
+<?php
+include "conn.php";
+session_start();
+
+if (!isset($_SESSION['username']) || $_SESSION['role'] !== 'admin') {
+    // Redirect the user to another page or display an error message
+    header("Location: unauthorized.php");
+    exit(); // Ensure script stops executing after redirection
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<link rel="stylesheet" href="Css/main.css">
+	<link rel="stylesheet" href="Css/style.css">
+	<link rel="stylesheet" href="Css/tweet_box.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/emojionearea/3.4.2/emojionearea.min.css" />
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <script defer src="navscript.js"></script>
+</head>
+<body>
+    <?php require "adminNav.php" ?>
+    <header class="adminContent">
+    <h1>AdminDashboard</h1>
+    </header>
+
+
+</body>
+</html>
